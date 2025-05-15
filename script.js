0cf514f78fa185f6c020c902c5df8ea42605547e
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-15 17:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function click(){
+
+	const firstName=document.getElementById("fname").value.trim()
+	const LastName=document.getElementById("lname").value.trim()
+	const phone=document.getElementById("phone").value.trim()
+	const emailid=document.getElementById("emailid").value.trim()
+
+	const msg=`First Name: ${firstName} Last Name: ${LastName} Phone Number: ${phone} Email ID: ${emailid}`
+	
+	alert('msg');
+}
